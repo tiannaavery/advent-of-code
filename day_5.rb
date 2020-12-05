77a08899017e68efc175c2f0b7b3faaa5b68a49e
Tianna Avery <tiannaavery@gmail.com>
2020-12-05 13:14:36 -0600

    Slightly improved day 5

--- day_5.rb
+++ day_5.rb
@@ -1,10 +1,10 @@
 # https://adventofcode.com/2020/day/5
 
-$num_rows = Array.new(128) { |i| i + 1 }
-$num_columns = Array.new(8) { |i| i + 1 }
+$num_rows = 128
+$num_cols = 8
 
 def boarding_passes
-  File.new("day_5_boarding_passes.txt").readlines
+  @boarding_passes ||= File.new("day_5_boarding_passes.txt").readlines
 end
 
 def navigate_aisles(boarding_pass, rows, cols)
@@ -13,7 +13,6 @@ def navigate_aisles(boarding_pass, rows, cols)
       puts "uh oh"
     end
 
-    # puts "#{rows[0]}, #{cols[0]}"
     return rows[0] * 8 + cols[0]
   end
 
@@ -27,42 +26,44 @@ def navigate_aisles(boarding_pass, rows, cols)
   when "R"
     navigate_aisles(boarding_pass[1..], rows, cols.last(cols.length/2))
   else
-    puts "where are you going?! the character here is #{boarding_pass[0]}"
+    puts "huh?!"
+  end
+end
+
+def filled_seats
+  @filled_seats ||= begin
+    seats = []
+
+    boarding_passes.each do |pass|
+      seat_num = navigate_aisles(pass.strip, Array.new($num_rows) { |i| i}, Array.new($num_cols) { |i| i })
+      seats[seat_num] = seat_num
+    end
+
+    seats
   end
 end
 
 def highest_seat
-  highest_seat = 0
-
-  boarding_passes.each do |pass|
-    seat_number = navigate_aisles(pass.strip, Array.new(128) { |i| i}, Array.new(8) { |i| i })
-    highest_seat = seat_number if seat_number > highest_seat
-  end
-
-  highest_seat
+  filled_seats.compact.max
 end
 
 def find_empty_seat
-  filled_seats = []
-
-  boarding_passes.each do |pass|
-    seat_num = navigate_aisles(pass.strip, Array.new(128) { |i| i}, Array.new(8) { |i| i })
-    filled_seats[seat_num] = seat_num
-  end
-
-  my_seat = []
+  my_seat = 0
 
   filled_seats.each_index do |i|
     seat = filled_seats[i]
-    my_seat << i if seat.nil? && filled_seats[i - 1] && filled_seats[i + 1]
+    if seat.nil? && filled_seats[i - 1] && filled_seats[i + 1]
+      my_seat = i
+      break
+    end
   end
 
-  my_seat[0]
+  my_seat
 end
 
 def main
   puts "the highest seat number... it must be #{highest_seat}" # 938
-  puts "by process of elimination, i'm gonna assume my seat is #{find_empty_seat}"
+  puts "by process of elimination, i'm gonna assume my seat is #{find_empty_seat}" #696
 end
 
 main()
